778577212edc0beb068ddca1ae22e2dfc52636fc
Marcin Krysiński <marcin.krysik11@gmail.com>
2018-09-10 16:26:26 +0200

    localstorage for letters, tries variable and pictures

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 //TODO:
 // dodac żeby nie traktowal spacji jako znaku +
-// dodac APi aby losował slowo do zgadniecia
+// dodac APi aby losował slowo do zgadniecia :/ 
 // przechowywanie aktualnego stanu gry do localstorage
-// reset button  
-// dodac babela
+// reset button  +
+// dodac babela 
 
 //password
 const word = 'słowo to telewizor';
 
 //each char of password
 const chars = word.toLowerCase().split(''); 
-console.log(chars)
 // ['s', 'ł', 'o', 'w', 'o']
 
 const password = document.getElementById('password');
 const score = document.getElementById('score');
-
-
-const hideArr = new Array(chars.length);
+const hangmanPic = document.querySelector('.hangmanPic');
+let tries = 0;
+let hideArr = new Array(chars.length);
 
 function hidePassword() {
   for(let i=0; i<chars.length; i++) {
@@ -28,6 +27,21 @@ function hidePassword() {
 }
 hidePassword()
 
+function localstorageGetItems() {
+  if(typeof(Storage) !== "undefined") {
+    if (localStorage.getItem('tries') != null) {
+      tries = parseInt(localStorage.getItem('tries'));
+      hangmanPic.src = `./img/s${tries}.jpg`;
+    }
+    if(localStorage.getItem('currPass') != null) {
+      let curPass =  localStorage.getItem('currPass').split(',')
+      hideArr = curPass;
+      password.innerHTML = hideArr.join('')
+      
+    }
+  }
+}
+localstorageGetItems();
 
 function indexOfChar(array, element) {
   let counts = []
@@ -39,13 +53,14 @@ function indexOfChar(array, element) {
   return counts
 }
 
-let tries = 0;
+
 
 function checkPassword() {
   if(chars.includes(this.innerHTML)) {
     let indexs = indexOfChar(chars, this.innerHTML);
     indexs.forEach(index => {
       hideArr[index] = this.innerHTML;
+      localStorage.setItem('currPass', hideArr);
       return password.innerHTML = hideArr.join('')
     })
   }
@@ -53,6 +68,7 @@ function checkPassword() {
     return
   }
 }
+
 
 function changeStyles() {
   if(!hideArr.includes('_')) score.innerHTML = "Wygrałes !"
@@ -63,34 +79,39 @@ function changeStyles() {
     this.style.backgroundColor = '#00ff00';
   }
   else {
-    const hangmanPic = document.querySelector('.hangmanPic');
     const picContainer = document.querySelector('.picture');
     if(tries>=9) {
       score.textContent = 'PRZEGRAŁEŚ';
       picContainer.innerHTML = '';
-      buttons.forEach(btn => btn.removeEventListener('click', checkPassword)) 
+      buttons.forEach(btn => btn.removeEventListener('click', checkPassword));
     }
     this.style.backgroundColor = '#ff0000';
     tries++ 
     hangmanPic.src = `./img/s${tries}.jpg`;
     this.removeEventListener('click', checkPassword);
     this.removeEventListener('click', changeStyles);
+    // localstorage
+    localStorage.setItem('tries', tries);
   }
 }
 
+
 //buttons
-const nodeButtons = document.querySelectorAll('button');
+const nodeButtons = document.querySelectorAll('.letter');
 const buttons = [...nodeButtons];
 const addClickEvent = buttons.forEach(btn => { 
   btn.addEventListener('click', checkPassword)
   btn.addEventListener('click', changeStyles)
 });
 
+// reset button
+const resetBtn = document.querySelector('.reset');
 
-
-
-
-
+document.querySelector('.reset')
+  .addEventListener('click', function() {
+    localStorage.clear()
+    location.reload();
+  })
 
 
 
