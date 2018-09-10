b511dbbb03ef29925a9f5befc197409728cc53a0
Marcin Krysiński <marcin.krysik11@gmail.com>
2018-09-10 12:57:00 +0200

    clean up the code and adding the functionality of several words in the password

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
+//TODO:
+// dodac żeby nie traktowal spacji jako znaku +
+// dodac APi aby losował slowo do zgadniecia
+// przechowywanie aktualnego stanu gry do localstorage
+// reset button  
+// dodac babela
+
 //password
-const word = 'yo';
+const word = 'słowo to telewizor';
 
 //each char of password
 const chars = word.toLowerCase().split(''); 
+console.log(chars)
 // ['s', 'ł', 'o', 'w', 'o']
 
 const password = document.getElementById('password');
-const hiddenPassword = document.getElementById('hidePassword');
+const score = document.getElementById('score');
 
 
 const hideArr = new Array(chars.length);
 
 function hidePassword() {
   for(let i=0; i<chars.length; i++) {
-    hideArr[i] = '_'
+    hideArr[i] = '_';
+    if(chars[i] == ' ') hideArr[i] = ' '
   }
-  password.innerHTML = hideArr.join(' ');
+  password.innerHTML = hideArr.join('');
 }
 hidePassword()
 
@@ -32,14 +41,12 @@ function indexOfChar(array, element) {
 
 let tries = 0;
 
-passwordArr = new Array(word.length);
-
 function checkPassword() {
   if(chars.includes(this.innerHTML)) {
     let indexs = indexOfChar(chars, this.innerHTML);
     indexs.forEach(index => {
       hideArr[index] = this.innerHTML;
-      return password.innerHTML = hideArr.join(' ')
+      return password.innerHTML = hideArr.join('')
     })
   }
   else {
@@ -48,7 +55,8 @@ function checkPassword() {
 }
 
 function changeStyles() {
-  if(!hideArr.includes('_')) {hiddenPassword.innerHTML = "Wygrałes !"}
+  if(!hideArr.includes('_')) score.innerHTML = "Wygrałes !"
+  
   if(chars.includes(this.innerHTML)) {
     this.removeEventListener('click', checkPassword);
     this.removeEventListener('click', changeStyles);
@@ -58,7 +66,7 @@ function changeStyles() {
     const hangmanPic = document.querySelector('.hangmanPic');
     const picContainer = document.querySelector('.picture');
     if(tries>=9) {
-      password.textContent = 'PRZEGRAŁEŚ';
+      score.textContent = 'PRZEGRAŁEŚ';
       picContainer.innerHTML = '';
       buttons.forEach(btn => btn.removeEventListener('click', checkPassword)) 
     }
